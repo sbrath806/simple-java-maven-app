125d89babf35365f03bddf282cf8c6bd039b5cc3
sbrath806 <53057291+sbrath806@users.noreply.github.com>
2019-07-19 11:47:07 -0400

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -11,7 +11,7 @@ public class App
     public App() {}
 
     public static void main(String[] args) {
-        System.out.println("Lamda Not Supported.");
+        System.out.println("Hello World!");
         System.out.println(new App().getMessage());	
     }
 
